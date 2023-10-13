037a5cb2a7f080ff82366b6d1fd345c1fb9fe076
Francelys Raquel Vanega Rodríguez <147260983+francelys01@users.noreply.github.com>
2023-10-13 10:08:53 -0600

    Update Ejercicio_1.cs
    Entrega de trabajo

--- Ejercicio_1.cs
+++ Ejercicio_1.cs
@@ -3,3 +3,26 @@
 
 Escribe un programa
 que muestre los números pares del 2 al 20. */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numeros_pares1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i <= 20; i = i + 2)
+            {
+
+                Console.WriteLine(i);
+            }
+            Console.Read();
+        
+        }
+    }
+}
