ecfcb1403147068b91fa06b29b6c0dc1d2d08c36
Francelys Raquel Vanega Rodríguez <147260983+francelys01@users.noreply.github.com>
2023-10-13 10:16:52 -0600

    Update Ejercicio_4.cs
    Entrega de Trabajo

--- Ejercicio_4.cs
+++ Ejercicio_4.cs
@@ -3,3 +3,29 @@
 
 Crea un programa que genere y muestre el
 resultado factorial de un numero. */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el número");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int factorial = 1;
+            for (int i = 1; i <= num; i++)
+            {
+                factorial = factorial * i;
+            }
+            Console.WriteLine("El factorial de " + num + " es " + factorial);
+            Console.ReadLine();
+        }
+    }
+}
+
