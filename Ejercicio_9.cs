5259df9b6a692a7c9f696708832b91daeefeea4f
Francelys Raquel Vanega Rodríguez <147260983+francelys01@users.noreply.github.com>
2023-10-13 11:01:53 -0600

    Update Ejercicio_9.cs
    Entrega de trabajo

--- Ejercicio_9.cs
+++ Ejercicio_9.cs
@@ -6,3 +6,43 @@ Dado dos números, muestra el número mayor
 utilizando una estructura if
 
 */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mayor_de_dos_números
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1;
+            double n2;
+
+            Console.WriteLine("Número uno");
+            n1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Número dos");
+            n2 = Convert.ToDouble(Console.ReadLine());
+
+            if (n1 > n2)
+            {
+                Console.WriteLine("El número " + n1 + " es el mayor");
+            }
+            else if (n1 < n2)
+            {
+                Console.WriteLine("El número " + n2 + " es el mayor");
+            }
+            else
+            {
+                Console.WriteLine("Los números son iguales.");
+            }
+            Console.ReadLine();
+             
+        }
+    }
+}
+
