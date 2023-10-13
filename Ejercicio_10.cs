96e27c9ad6d664da31a902898d1a5df70653df5b
Francelys Raquel Vanega Rodríguez <147260983+francelys01@users.noreply.github.com>
2023-10-13 11:06:27 -0600

    Update Ejercicio_10.cs
    Entrega de trabajo

--- Ejercicio_10.cs
+++ Ejercicio_10.cs
@@ -6,3 +6,26 @@ Positivo, Negativo o Cero
 Escribe un programa en C# que determine si
 un número entero dado es positivo, negativo o cero.
 */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabla_de_multiplicar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int número;
+            Console.WriteLine("Ingrese un número");
+            número = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= 12; i++)
+            {
+                Console.WriteLine(número + " x " + i + " = " + número * i);
+            }
+              Console.ReadKey();
+        }
+    }
