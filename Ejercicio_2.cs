aeef3dc2038acc704f3ce5616e27a93b14563a2f
Francelys Raquel Vanega Rodríguez <147260983+francelys01@users.noreply.github.com>
2023-10-13 10:12:11 -0600

    Update Ejercicio_2.cs
    Entrega de trabajo.

--- Ejercicio_2.cs
+++ Ejercicio_2.cs
@@ -2,4 +2,27 @@
 
 
 Crea un programa que calcule y muestre la
-suma de los dígitos de un número ingresado por el usuario.*/
+suma de los dígitos de un número ingresado por el usuario.*/using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace suma_de_dos_digitos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c = 0;
+            Console.WriteLine("Ingrese un dígito");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese un dígito");
+            b = int.Parse(Console.ReadLine());
+            c = a + b;
+            Console.WriteLine("La suma es: " + c);
+            Console.ReadLine();
+        }
+    }
+}
+
