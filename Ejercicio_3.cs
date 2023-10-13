f1eb3287dd235ca2f3e802321707812f9a51f01a
Francelys Raquel Vanega Rodríguez <147260983+francelys01@users.noreply.github.com>
2023-10-13 10:14:25 -0600

    Update Ejercicio_3.cs
    Entrega de trabajo.

--- Ejercicio_3.cs
+++ Ejercicio_3.cs
@@ -3,3 +3,42 @@
 
 Escribe un
 programa que determine si un número dado es primo o no.*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numero_Primo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresar número : ");
+            int número = int.Parse(Console.ReadLine());
+            int divisor = 1, contador = 0;
+            while (divisor <= número) 
+            {
+                if (número % divisor == 0) 
+                {
+                    contador++;
+                }
+                divisor++;
+            }
+            if (contador == 2)
+            {
+                Console.Write("El número es primo");
+            }
+            else
+            {
+                Console.WriteLine("El número no es primo");
+            }
+           
+            Console.ReadKey();
+
+        }
+    }
+}
+
